8b605cfcf5153c0769e00b640c136e554593c346
Alina Potapova <alinapotapova@192-168-0-101.local>
2023-05-10 00:37:15 +0300

    Add audio during recording view

--- MemoriesSwiftUI/View/MainViews/InputAccesseryView.swift
+++ MemoriesSwiftUI/View/MainViews/InputAccesseryView.swift
@@ -18,6 +18,7 @@ struct InputAccesseryView: View {
     @ObservedObject var audioPlayerVM: AudioPlayerVM
     
     @State var showCameraView: Bool = false
+    @State var isRecording: Bool = false
     @State var showImagePicker: Bool = false
     @State var showImagePickerInTF: Bool = false
 
@@ -47,51 +48,13 @@ struct InputAccesseryView: View {
     
     var body: some View {
         VStack(spacing: 0) {
-            if showCameraView {
-                CameraView()
-                    .environmentObject(cameraModel)
-                    .background(BlurView(style: .systemMaterialDark, intensity: 1))
-                    .ignoresSafeArea()
-            }
             HStack(spacing: 0) {
                 if !isKeyboardPresented {
-                    Button {
-                        PHPhotoLibrary.requestAuthorization { status in
-                            switch status {
-                            case .authorized, .limited:
-                                self.showImagePicker.toggle()
-                                break
-                            case .denied, .restricted, .notDetermined:
-                                self.showAlert.toggle()
-                                break
-                            @unknown default:
-                                break
-                            }
-                        }
-                    } label: {
-                        HStack(spacing: 0) {
-                            Image(UI.Icons.attachments)
-                                .padding(16)
-                                .foregroundColor(.cW)
-                            Rectangle()
-                                .frame(width: 2, height: 44)
-                                .foregroundColor(.c3)
-                        }
+                    if !isRecording {
+                        attachmentBtnView
+                    } else {
+                        recordingDuration
                     }
-                    .alert(isPresented: $showAlert) {
-                        Alert(title: Text(UI.Alearts.alert_title),
-                              message: Text(UI.Alearts.message_text),
-                              primaryButton: .default(Text(UI.Alearts.primaryBtn_text)),
-                              secondaryButton: .default(Text(UI.Alearts.secondaryBtn_text), action: {
-                                    guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else { return }
-                                          UIApplication.shared.open(settingsURL)
-                              }))
-                    }
-                    .transition(.move(edge: .leading))
-                    .sheet(isPresented: $showImagePicker) {
-                        ImagesPicker(selections: $selectedItems, selectionsVideo: $selectionsvideo, addFunc: withAnimation { addItem })
-                    }
-                    
                 }
                 VStack {
                     if !selectedItems.isEmpty && isKeyboardPresented {
@@ -109,7 +72,6 @@ struct InputAccesseryView: View {
                                         })
                                         .offset(x: 18, y: -18)
                                     )
-                                
                             }
                             Spacer()
                         }
@@ -126,42 +88,132 @@ struct InputAccesseryView: View {
                     }, media: {
                         self.showImagePickerInTF.toggle()
                     })
-                    .transition(.scale.animation(.easeInOut(duration: 0.01)))
+//                    .transition(.scale)
+//                    .animation(.easeInOut(duration: 1), value: isKeyboardPresented)
+
                     .padding(.horizontal, 8)
                     .frame(height: containerHeight <= 150 ? containerHeight : 150)
                     .sheet(isPresented: $showImagePickerInTF) {
                         ImagesPicker(selections: $selectedItems, selectionsVideo: $selectionsvideo, addFunc: {})
                     }
-                }
-
-                if !isKeyboardPresented {
-                    Button {
-//                        self.showCameraView.toggle()
-                        
-                        if audioRecorder.isRecording {
-                            stopRecording()
-                        } else {
-                            startRecording()
+                    .onTapGesture {
+                        withAnimation {
+                            isKeyboardPresented = true
                         }
-                    } label: {
-                        HStack(spacing: 0) {
-                            Rectangle()
-                                .frame(width: 2, height: 44)
-                                .foregroundColor(.c3)
-
-                            Image(UI.Icons.audio_message)
-                                .padding(16)
-                                .foregroundColor(.cW)
-                        }
-                    }.transition(.move(edge: .trailing))
+                    }
+                }.opacity(isRecording ? 0 : 1)
                 
+                if !isKeyboardPresented {
+                    audioBtnView
                 }
             }
-            .background(isKeyboardPresented ? .white : Color.c2)
-            .transition(.opacity)
+            .background(backgroundColor)
+            .animation(.default, value: isKeyboardPresented)
             .cornerRadius(isKeyboardPresented ? 8 : 16, corners: isKeyboardPresented ? [.topLeft, .topRight] : [.allCorners])
             .padding(.horizontal, isKeyboardPresented ? 0 : 16)
         }
+    }
+    
+    private var backgroundColor: Color {
+        if isKeyboardPresented && !isRecording {
+            return .white
+        } else if !isKeyboardPresented && !isRecording {
+            return Color.c2
+        } else {
+            return .c1
+        }
+    }
+    
+    var recordingDuration: some View {
+        HStack(spacing: 8) {
+            if let audioRecorder = audioRecorder.audioRecorder, audioRecorder.isRecording {
+                TimelineView(.periodic(from: .now, by: 1)) { _ in
+                    Text("-\(DateComponentsFormatter.positional.string(from: audioRecorder.currentTime) ?? "0:00")")
+                        .font(.bodyText(15))
+                        .foregroundColor(.c7)
+                        .frame(width: 50)
+                }
+            }
+            Circle()
+                .frame(width: 10)
+                .foregroundColor(.c5)
+        }.padding(.leading, 16)
+    }
+    
+    var attachmentBtnView: some View {
+        Button {
+            PHPhotoLibrary.requestAuthorization { status in
+                switch status {
+                case .authorized, .limited:
+                    self.showImagePicker.toggle()
+                    break
+                case .denied, .restricted, .notDetermined:
+                    self.showAlert.toggle()
+                    break
+                @unknown default:
+                    break
+                }
+            }
+        } label: {
+            HStack(spacing: 0) {
+                Image(UI.Icons.attachments)
+                    .padding(16)
+                    .foregroundColor(.cW)
+                Rectangle()
+                    .frame(width: 2, height: 44)
+                    .foregroundColor(.c3)
+            }
+        }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text(UI.Alearts.alert_title),
+                  message: Text(UI.Alearts.message_text),
+                  primaryButton: .default(Text(UI.Alearts.primaryBtn_text)),
+                  secondaryButton: .default(Text(UI.Alearts.secondaryBtn_text), action: {
+                        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else { return }
+                              UIApplication.shared.open(settingsURL)
+                  }))
+        }
+        .transition(.scale)
+//        .animation(.none, value: isKeyboardPresented)
+
+        .sheet(isPresented: $showImagePicker) {
+            ImagesPicker(selections: $selectedItems, selectionsVideo: $selectionsvideo, addFunc: withAnimation { addItem })
+        }
+        .opacity(isRecording ? 0 : 1)
+    }
+    
+    var audioBtnView: some View {
+        HStack(spacing: 0) {
+            Rectangle()
+                .frame(width: 2, height: 44)
+                .foregroundColor(.c3)
+                .opacity(isRecording ? 0 : 1)
+            ZStack {
+                Image(UI.Icons.audio_message)
+                    .padding(16)
+                    .foregroundColor(isRecording ? .c5 : .cW)
+            }
+        }
+        .transition(.scale)
+        .onLongPressGesture(minimumDuration: 0.01) {
+            if !audioRecorder.isRecording {
+                startRecording()
+                withAnimation {
+                    self.isRecording = true
+                }
+            }
+         }
+         .simultaneousGesture(
+             DragGesture(minimumDistance: 0)
+                .onEnded{ _ in
+                    if audioRecorder.isRecording {
+                        stopRecording()
+                        withAnimation {
+                            self.isRecording = false
+                        }
+                    }
+                }
+         )
     }
     
     private func addItem() {
@@ -210,3 +262,13 @@ struct InputAccesseryView: View {
         audioRecorder.stopRecording(chapter: chapter)
     }
 }
+
+
+
+
+//if showCameraView {
+//    CameraView()
+//        .environmentObject(cameraModel)
+//        .background(BlurView(style: .systemMaterialDark, intensity: 1))
+//        .ignoresSafeArea()
+//}
