d17313c0893b2cf5f9e4bed97e42bcaba90c41d6
Alina Potapova <alinapotapova@MacBook-Pro.local>
2023-11-01 15:01:58 +0300

    Add StatusMO class to track user activity

--- MemoriesSwiftUI/Models/StatusMO.swift
+++ MemoriesSwiftUI/Models/StatusMO.swift
@@ -1,8 +1,47 @@
 //
 //  StatusMO.swift
 //  MemoriesSwiftUI
-//
+// 
 //  Created by Alina Potapova on 24.10.2023.
 //
 
 import Foundation
+import CoreData
+import SwiftUI
+
+@objc(StatusMO)
+public class StatusMO: NSManagedObject {
+    @NSManaged public var value: Int16
+    @NSManaged public var isChanged: Bool
+}
+
+extension StatusMO {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<StatusMO> {
+        return NSFetchRequest<StatusMO>(entityName: "StatusMO")
+    }
+    
+    @nonobjc public class func resultsController(moc: NSManagedObjectContext) -> NSFetchedResultsController<StatusMO> {
+        let request = NSFetchRequest<StatusMO>(entityName: "StatusMO")
+        return NSFetchedResultsController(fetchRequest: request, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+    }
+     
+    public var safeValue: Int {
+        get { Int(value) }
+        set { value = Int16(newValue) }
+    }
+    
+    public var safeIsChanged: Bool {
+        get { isChanged }
+        set { isChanged = newValue }
+    }
+
+
+    public var safeStringValue: String {
+        switch value {
+        case 0...7:
+            return "\(value)"
+        default:
+            return "inactive-long-time"
+        }
+    }
+}
